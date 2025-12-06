255444067189c57938f3c046c90ff3bd7a9a5db6
Hemanth-33341 <135092425+Hemanth-33341@users.noreply.github.com>
2025-12-06 20:29:07 +0530

    second commit

--- crm_website/views.py
+++ crm_website/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        username = request.POST.get('user_name')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have logged in sucessfully!")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error logging in...")
+            return redirect('home')
+    
+    return render(request, 'crm_website/home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
--- website/settings.py
+++ website/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
+    'crm_website',
 ]
 
 MIDDLEWARE = [
